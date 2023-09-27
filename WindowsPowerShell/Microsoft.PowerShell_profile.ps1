6c979a53058d4228902680e8f345f8af2c8eeda5
Garve Hays <gkhays@gmail.com>
2023-09-27 17:29:54 -0500

    Add theme and alias for which

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,5 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerline.omp.json" | Invoke-Expression
+
 Set-Alias dc docker-compose
+New-Alias which get-command
 New-Alias -Name git -Value "$Env:ProgramFiles\Git\bin\git.exe"
